8fb2d2d242ed7689b4fd3554a07dfc17127fb670
Léo Chappart <lchappart>
2025-02-11 14:12:09 +0100

    Exo1

---
+++ Methods/src/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public int calculerSomme(int a, int b) {
+        return a + b;
+    }
+    public double calculerSomme(double a, double b) {
+        return a + b;
+    }
+}
---
+++ Methods/src/Exo1.java
@@ -0,0 +1,7 @@
+public class Exo1 {
+    public static void main(String[] args) {
+        Addition a = new Addition();
+        System.out.println(a.calculerSomme(1, 2));
+        System.out.println(a.calculerSomme(1.5, 2.7));
+    }
+}
