e55012e7534f51be71c9f96a4a642b8a539ba7e3
Léo Chappart <lchappart>
2025-02-11 12:25:34 +0100

    Init

---
+++ Variables/src/Exo2.java
@@ -0,0 +1,11 @@
+public class Exo2 {
+    public static void main(String[] args) {
+        /*
+        My
+        Java
+        Code
+        Every
+        Day
+         */
+    }
+}
---
+++ Variables/src/Exo3.java
@@ -0,0 +1,26 @@
+public class Exo3 {
+    public static void main(String[] args) {
+
+        // primitive data types
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+        float myFloat = 3.4028235E38f;
+        double myDouble = 1.7976931348623157E308;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+        // reference data types
+        String myString = "Hello";
+        Integer myInteger = 42;
+        Double myWrappedDouble = 3.14;
+        Object myObject = new Object();
+
+        System.out.println(myByte);
+
+        System.out.println(myShort);
+        System.out.println(myObject);
+
+    }
+}
